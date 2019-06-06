3f919c7021a2a9ecccbfd60004a2c78388bbbc65
EmerHoff <emerson_h2010@hotmail.com>
2019-06-06 00:28:45 -0300

    Corrigido

--- Hamming.py
+++ Hamming.py
@@ -10,7 +10,7 @@ def MultiGeradora(vetor, MatrizGeradora):
         aux = aux % 2
         resul.append(aux)
         aux = 0
-    print('Palavra gerada {}' .format(resul))
+    #print('Palavra gerada {}' .format(resul))
     return resul
 
 
@@ -37,67 +37,90 @@ def Verificadora(verifica):
         aux += 2
     if verifica[3] == 1:
         aux += 1
-    print('ResulVerificacao {}' .format(aux))
+    #print('ResulVerificacao {}' .format(aux))
     return aux
 
 
 def Correcao(ValorResultante, palavra):
 
     if palavra[ValorResultante - 1] == 1:
-        palavra[ValorResultante - 1] = 0
+        palavra[ValorResultante - 1] = str(0)
     else:
-        palavra[ValorResultante - 1] = 1
+        palavra[ValorResultante - 1] = str(1)
     return palavra
 
 
 def CalculaG(substring):
-    VarG = substring[0]
+    VarG = bool(substring[0])
     for i in range(1, len(substring)):
-        VarG = bool(VarG) ^ bool(substring[i])
-    #print(int(VarG))
+        if VarG == True and substring[i] == True:
+            VarG = False
+        if VarG == False and substring[i] == True:
+            VarG = True
+        if VarG == True and substring[i] == False:
+            VarG = True
+        if VarG == False and substring[i] == False:
+            VarG = False
 
     return int(VarG)
 
 
 
 def Decodifica(substring, MatrizReconhecedora):
-    ValorGOriginal = substring[3]
-    print('ValorGOriginal {}' .format(ValorGOriginal))
+    PalavraFinal = list()
+    print('L = Lixo\t G = Bit G\t P = Paridade\t D = Dados')
+    print('            [ L ,  L ,  L ,  D ,  D ,  D ,  D ,  P ,  D ,  D ,  D ,  P ,  D ,  P ,  P ,  G ]')
+    print('Palavra Lida{}' .format(substring))
+
+    ValorGOriginal = substring[15]
+    #print('ValorGOriginal {}' .format(ValorGOriginal))
     NovaString = list()
 
-    for i in range(4, len(substring)):
+    for i in range(3, len(substring)-1):
         NovaString.append(substring[i])
 
-    print('NovaString {}' .format(NovaString))
+    NovaString.reverse()
     verifica = MultiVerificadora(NovaString, MatrizReconhecedora)
-    print('verifica {}' .format(verifica))
+    print('Verificacao {}' .format(verifica))
 
     ValorResultante = Verificadora(verifica)
 
-    ValorGRecalculado =CalculaG(NovaString)
+    ValorGRecalculado = CalculaG(NovaString)
+    #print('ValorGRecalculado {}' .format(ValorGRecalculado))
 
-    if ValorGRecalculado != ValorGOriginal:
-        print('Dois erros deu ruim')
+    if int(ValorGRecalculado) != int(ValorGOriginal):
+        print('Dois Erros! Deu Ruim')
         salvaPalavraModificada(substring)
     else:
         if ValorResultante == 0:
-            print('correto')
+            print('Palavra Correta')
             salvaPalavraModificada(substring)
         else:
             ValorCorrigido = Correcao(ValorResultante, NovaString)
-            print('Palavra Corrigida {}' .format(ValorCorrigido))
-            salvaPalavraModificada(ValorCorrigido)
+            PalavraFinal.append(str(0))
+            PalavraFinal.append(str(0))
+            PalavraFinal.append(str(0))
+            
+            ValorCorrigido.reverse()
+            for i in range(0, len(ValorCorrigido)):
+                PalavraFinal.append(ValorCorrigido[i])
+
+            PalavraFinal.append(str(ValorGRecalculado))
+            print('L = Lixo\t G = Bit G\t P = Paridade\t D = Dados')
+            print('                 [ L ,  L ,  L ,  D ,  D ,  D ,  D ,  P ,  D ,  D ,  D ,  P ,  D ,  P ,  P ,  G ]')
+            print('Palavra Corrigida{}' .format(PalavraFinal))
+            salvaPalavraModificada(PalavraFinal)
 
 
 def salvaPalavra(PalavraFinal):
-    arq = open('/home/fernando/Area de Trabalho/DadosFinais.thc','a')
+    arq = open('DadosFinais.thc','a')
     for i in range(0, len(PalavraFinal)):
         arq.writelines(str(PalavraFinal[i]))
     arq.close()
 
 
 def salvaPalavraModificada(PalavraFinal):
-    arq = open('/home/fernando/Area de Trabalho/DadosFinaisModificados.thc','a')
+    arq = open('DadosFinaisModificados.thc','a')
     for i in range(0, len(PalavraFinal)):
         arq.writelines(str(PalavraFinal[i]))
     arq.close()
@@ -107,19 +130,29 @@ def Codifica(substring, MatrizGeradora):
 
     PalavraFinal = list()
     palavra = list()
+    #print('Substring {}' .format(substring))
+    #palavra.reverse()
+    substring.reverse()
     palavra = MultiGeradora(substring, MatrizGeradora)
     G = CalculaG(palavra)
     print('Palavra {}' .format(palavra))
+
     PalavraFinal.append(0)
     PalavraFinal.append(0)
     PalavraFinal.append(0)
-    PalavraFinal.append(G)
+    
     palavra.reverse()
     for i in range(0, len(palavra)):
         PalavraFinal.append(palavra[i])
+    
+    PalavraFinal.append(G)
+    
     salvaPalavra(PalavraFinal)
+    
+    #palavra.reverse()
+    
     print('L = Lixo\t G = Bit G\t P = Paridade\t D = Dados')
-    print('             [L, L, L, G, P, P, D, P, D, D, D, P, D, D, D, D]')
+    print('             [L, L, L, D, D, D, D, P, D, D, D, P, D, P, P, G]')
     print('PalavraFinal {}' .format(PalavraFinal))
 
 
@@ -160,9 +193,8 @@ if str(sys.argv[2]) == '-w':
     for j in range(0, len(string)):
         substring.append(string[j])
         if len(substring) == 8:
+            #print('Substring {}' .format(substring))
             Codifica(substring, MatrizGeradora)
-            #print(substring)
-
             substring.clear()
     ref_arquivo.close()
 
